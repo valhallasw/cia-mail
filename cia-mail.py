a8966edf43998d759165e15b06f6963e917458e5
Merlijn van Deen <valhallasw@gmail.com>
2009-07-28 00:40:55 +0200

    Initial release of version 0.2 of the cia-mail script. Now includes support for encoded subjects and senders.

---
+++ cia-mail.py
@@ -0,0 +1,49 @@
+#!/usr/local/bin/python
+#
+# Copyright (C) Merlijn van Deen <valhallasw@gmail.com>, 2009
+#
+# Distributed under the terms of the MIT license.
+#
+
+import sys, time
+from email.Parser import Parser
+from email.Header import Header, decode_header
+from xml.sax.saxutils import escape
+from xmlrpclib import ServerProxy
+
+fp = open('test')
+e = Parser().parse(fp)
+
+# Stupid email library. This parses all headers into nice unicode strings...
+headers = dict([(header, Header(*decode_header(e[header])[0]).__unicode__()) for header in e.keys()])
+
+author = headers['From']
+author = author[:author.find('<')].strip() # remove email address
+
+subject = headers['Subject']
+
+message = """
+<message>
+  <generator>
+    <name>CIA Python client for mail</name>
+    <version>0.2</version>
+  </generator>
+  <source>
+    <project>%(project)s</project>
+  </source>
+  <timestamp>%(timestamp)s</timestamp>
+  <body>
+    <commit>
+      <author>%(author)s</author>
+      <log>%(subject)s</log>
+    </commit>
+  </body>
+</message>""" % {
+    'project'  : escape(sys.argv[1]),
+    'timestamp': int(time.time()),
+    'author'   : escape(author.encode('utf-8')),
+    'subject'  : escape(subject.encode('utf-8'))
+    }
+
+print message
+print ServerProxy('http://cia.vc/RPC2').hub.deliver(message)
